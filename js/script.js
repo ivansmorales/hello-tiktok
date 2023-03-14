9937aed56083922ae8e6ead20bbad0371feac3a7
ivansmorales <ivann_mg@hotmail.com>
2023-03-13 20:34:06 -0600

    second commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("boton");
+const titulo = document.getElementById("titulo");
+
+boton.addEventListener("click", function() {
+titulo.innerHTML = "Título cambiado";
+});
